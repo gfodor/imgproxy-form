919470004a2abc86607ebfd0ae0bace30f6c0af3
Greg Fodor <gfodor@gmail.com>
2018-06-20 16:40:29 +0000

    Bugfixes, remove gravity, support gif

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ function hex2a(hexx) {
   return str
 }
 
-// URL generator logic
 function generateProxyUrl(opts) {
-  var encoded_url = btoa(opts.url).replace("=", "").replace('/', '_').replace('+', '-')
+  var encoded_url = btoa(opts.url).replace(/=/g, "").replace(/\//g, '_').replace(/\+/g, '-')
   var path = "/" + opts.resize + "/" + opts.width + "/" + opts.height + "/" +
-             opts.gravity + "/" + opts.enlarge + "/" + encoded_url + "." + opts.extension
+             opts.enlarge + "/" + encoded_url + "." + opts.extension
   var shaObj = new jsSHA("SHA-256", "BYTES")
   shaObj.setHMACKey(opts.key, "HEX")
   shaObj.update(hex2a(opts.salt))
   shaObj.update(path)
-  var hmac = shaObj.getHMAC("B64").replace("=", "").replace('/', '_').replace('+', '-')
+  var hmac = shaObj.getHMAC("B64").replace(/=/g, "").replace(/\//g, '_').replace(/\+/g, '-')
   return opts.proxy_url + "/" + hmac + path
 }
 
